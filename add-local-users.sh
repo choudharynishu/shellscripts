47147b64706629887537dc9c91c21ede6503f1cb
Nishu Choudhary <nishuchoudhary@gatech.edu>
2024-02-16 14:50:35 -0600

    redirected error message to stderr while suppressed all other output and error msgs

--- add-local-users.sh
+++ add-local-users.sh
@@ -7,15 +7,15 @@
 
 if [[ "${UID}" -ne 0 ]]
 then
-  echo 'Please run with sudo or as root'
+  echo 'Please run with sudo or as root' >&2
   exit 1 
 fi
 
 # Check if there is atleast one argument provided in the input. If not, tell user about usage
 if [[ "${#}" -eq 0 ]]
 then
-  echo "Usage: #{0} USERNAME [USERNAME]..."
-  echo "Create an account on the local system with the name of USER_NAME and NAME field for COMMENT"
+  echo "Usage: #{0} USERNAME [USERNAME]..." >&2
+  echo "Create an account on the local system with the name of USER_NAME and NAME field for COMMENT" >&2
   exit 1
 fi
 # Extract the username and name
@@ -23,14 +23,14 @@ USERNAME=${1}
 NAME=${2}
 
 # Add the user of specified name and username
-useradd -c "${NAME}" -m ${USERNAME}
+useradd -c "${NAME}" -m ${USERNAME} &> /dev/null
 
 # Check if the user was created successfully
 if [[ "${?}" -eq 0 ]]
 then
  echo 'User account creation successful!'
 else
- echo 'Account couldnt be created'
+ echo 'Account couldnt be created' >&2
  exit 1
 fi
 
@@ -42,18 +42,18 @@ SPECIAL_CHAR=$(echo ${ALPHANUMERIC} | fold -w1 | shuf)
 PASSWORD=$(date +%s%N | sha256sum | head -c27 )${RANDOM}${SPECIAL_CHAR}
 
 # Assign the generated password
-echo ${PASSWORD} | passwd --stdin ${USERNAME}
+echo ${PASSWORD} | passwd --stdin ${USERNAME} &> /dev/null
 
 if [[ "${?}" -eq 0 ]]
 then 
  echo 'Password assigned successfully'
 else
- echo 'Password creation unsuccessful'
+ echo 'Password creation unsuccessful' >&2
  exit 1
 fi
 
 # Force the user to change the password after first login
-passwd -e ${USERNAME}
+passwd -e ${USERNAME} &> /dev/null
 
 echo
 echo "Username: ${USERNAME}"
